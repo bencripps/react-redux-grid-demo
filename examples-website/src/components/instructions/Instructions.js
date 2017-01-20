293f16713d366cc91ea7ee89f9da79339793c4c1
brandon flowers <brandonflowers@gmail.com>
2017-01-20 12:21:58 -0500

    added more polish for a phase 1 release without 3 examples that do not work

---
+++ examples-website/src/components/instructions/Instructions.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Instructions.css';
+
+class Instructions extends Component {
+  render() {
+    return (
+        <div className="instructionsContainer">
+         <div className="instructions">
+            <p>
+            Whether you're building a Highscore table or an Enterprise user admin, <a href="https://github.com/bencripps/react-redux-grid" target="_blank">React-Data-Grid</a> will give you a highly configurable library that can draw tabular data. You can begin by installing:
+            </p> 
+            <code>npm install react-redux-grid --save</code>
+            <p>Then, this site will help you configure your desired table. All the examples are available to download from a separate <a href="https://github.com/bencripps/react-redux-grid-demo" target="_blank">Examples Github</a> where you can also contribute your own custom grid examples. 
+            </p>
+          </div>
+        </div>  
+    );
+  }
+}
+
+export default Instructions;
---
+++ examples-website/src/redux/reducers/features.js
@@ -0,0 +1,77 @@
+// all features
+export const featureTitles = [
+  "simple",
+  "complex",
+  "colRenderer",
+  "sticky",
+  "stress",
+  "editable",
+  "bootstrap",
+  "tree"
+];
+
+export const defaultFeatures = [
+  "💎 No Features Found"
+]
+
+export const simpleFeatures = [
+  "💎 Local and/or Remote Data Source",
+  "💎 Built-in Error Handling Module",
+  "⭐️ Handles Big Data (1000000+ Records)"
+]
+
+export const complexFeatures = [
+  "➖ 🌲 Flat List or Tree Structure ",
+  "💎 Local and/or Remote Data Source",
+  "💎 Built-in Error Handling Module",
+  "⭐️ Handles Big Data (1000000+ Records)"
+]
+
+export const colRendererFeatures = [
+  "➖ 🌲 Flat List or Tree Structure ",
+  "💎 Local and/or Remote Data Source",
+  "💎 Built-in Error Handling Module",
+  "⭐️ Handles Big Data (1000000+ Records)"
+]
+
+export const stickyFeatures = [
+  "💎 Select Multiple Rows ",
+  "💎 Local and/or Remote Data Source"
+]
+
+export const stressFeatures = [
+  "⭐️ Handles Big Data (1000000+ Records)"
+]
+
+export const editableFeatures = [
+  "💎 Edit Cells and Rows"
+]
+
+export const bootstrapFeatures = [
+  "➖ 🌲 Flat List or Tree Structure ",
+  "💎 Local and/or Remote Data Source",
+  "💎 Local and/or Remote Pagination",
+  "💪 Extensive Column Definitions",
+  "💎 Draggable Column Width/Resizing",
+  "💎 Draggable Column Ordering",
+  "💎 Sortable Columns",
+  "💎 Grid Action Menus",
+  "💎 Bulk Action Toolbar",
+  "💎 Selection Model (Single, MultiSelect, Checkbox)",
+  "💎 Event Handling for many DOM Events",
+  "👔 Extendable, Modular Style",
+  "💎 Loading Mask",
+  "💎 Built-in Error Handling Module",
+  "⭐️ Handles Big Data (1000000+ Records)"
+];
+
+
+export const treeFeatures = [
+  "➖ 🌲 Flat List or Tree Structure ",
+  "💎 Local and/or Remote Data Source",
+  "💎 Built-in Error Handling Module",
+  "⭐️ Handles Big Data (1000000+ Records)"
+]
+
+
+
